92cb440a1ef0640792b1619f9fd7c299a4d29175
Manvendra Singh <08singhmanvendra@gmail.com>
2024-02-08 16:39:39 +0530

    Color Switcher

---
+++ colorswitcher.js
@@ -0,0 +1,21 @@
+function red(){
+	var a=document.querySelector('body');
+	a.style.backgroundColor='red';
+}
+function cyan(){
+	var a=document.querySelector('body');
+	a.style.backgroundColor='cyan';
+}
+function black(){
+	var a=document.querySelector('body');
+	a.style.backgroundColor='#222';
+}
+function green(){
+	var a=document.querySelector('body');
+	a.style.backgroundColor='green';
+}
+function orange(){
+	var a=document.querySelector('body');
+	a.style.backgroundColor='orange';
+}
+
